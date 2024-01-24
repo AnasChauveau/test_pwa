3bca6355c51267c8c7ea103c0edd13895fa85c21
anas <anas38.gt@gmail.com>
2024-01-24 16:57:31 +0100

    test manifest

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,44 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    VitePWA({
+  plugins: [react(), VitePWA({
       registerType: 'autoUpdate',
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'test_pwa',
+        short_name: 'test_pwa',
+        theme_color: '#1d4676',
+        background_color: '#f69435',
+        display: 'fullscreen',
+        scope:'/',
+        start_url: '/',
+        description: 'me',
+        icons: [
+            {
+                "src": "/icon-192x192.png",
+                "sizes": "192x192",
+                "type": "image/png"
+            },
+            {
+                "src": "/icon-256x256.png",
+                "sizes": "256x256",
+                "type": "image/png"
+            },
+            {
+                "src": "/icon-384x384.png",
+                "sizes": "384x384",
+                "type": "image/png"
+            },
+            {
+                "src": "/icon-512x512.png",
+                "sizes": "512x512",
+                "type": "image/png"
+            }
+        ]
       }
     })
   ]
 })
+
